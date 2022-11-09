66299d6c006885177b153c63e119cf550ff5285c
Salman Dabbakuti <dabbakutisalman@gmail.com>
2022-11-10 00:27:49 +0530

    updated: move to polygon as ropsten deprecated

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ const abi = [
   "function getCount() view returns (uint256)",
   "event Count(string method, uint256 count, address caller)"
 ];
-const address = "0xDBEc9567B99d4441E03784731ba86E017B979206";
+const address = "0x1064191A9b2981CC3Af7038E1c4F24B244bb8152";
 
 $(document).ready(() => {
   // Connect to the blockchain
   if (window.ethereum) {
-    if (ethereum.networkVersion !== '3') {
+    if (ethereum.networkVersion !== '80001') {
       // if network is not ropsten, try switching to ropsten
       ethereum.request({
         method: 'wallet_switchEthereumChain',
-        params: [{ chainId: '0x3' }],
+        params: [{ chainId: '0x13881' }],
       }).catch((err) => {
         error(err.message);
       });
     }
-    log("Connected to the Ropsten test network.");
+    log("Connected to the Polygon mumbai network.");
 
     // ask metamask's permissions to access accounts and set default account
     ethereum.request({ method: 'eth_requestAccounts' })
@@ -59,26 +59,6 @@ $(document).ready(() => {
 
     // setup provider and contract
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const defaultProvider = ethers.getDefaultProvider();
-    const estimatedMergeBlock = 15537351;
-    defaultProvider.getBlock().then((currentBlock) => {
-      if (currentBlock.number > estimatedMergeBlock) {
-        // greet user after succesful merge
-        $('#merge-countdown').text('');
-        $('#merge-countdown').append(`<span>Merge Successful!. Welcome to The Merge Party! &#127881; &#129395; &#127881; &#129395;</span></br>`);
-      } else {
-        console.log('currentBlock: ', currentBlock);
-        const remainingBlocks = estimatedMergeBlock - currentBlock.number;
-        const estimatedTimeInSeconds = remainingBlocks * 14.3; // average 14.3 seconds per block
-        const date = new Date(null);
-        date.setSeconds(estimatedTimeInSeconds);
-        const estimatedTime = date.toISOString().slice(11, 19);
-        const [hours, minutes, seconds] = estimatedTime.split(':');
-        $('#merge-countdown').text('');
-        $('#merge-countdown').append(`${currentBlock.number}/${estimatedMergeBlock}: ${remainingBlocks} blocks to go. <br/>The Merge upgrade at around block 15,537,351 is scheduled to occur in <br/> ${hours} hours ${minutes} minutes ${seconds} seconds`);
-      }
-    });
-
     const signer = provider.getSigner();
     console.log(signer);
     const contract = new ethers.Contract(address, abi, signer);
